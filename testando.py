9c1e5b86deb2c0119aeed4b960531500f7abd592
DesejadoTerraNova <126709021+DesejadoTerraNova@users.noreply.github.com>
2023-03-31 16:37:04 -0300

    Add files via upload

--- testando.py
+++ testando.py
@@ -6,26 +6,40 @@
 # print(f"{year}/{month}NB-HE160")
 from tkinter import *
 from tkinter import ttk
+# import getpass
 
 class Janela:
-    def __init__(self, janin):
+    def __init__(self):
         # self.janela = ttk.Frame(janin, padding=10)
-        self.frm = ttk.Frame(janin, padding=10)
+        self.tk = Tk()
+        self.frm0 = ttk.Frame(self.tk, padding=10)
         self.grade = self.frm.grid()
         # self.titulo = ttk.Label(self.frm, text="Olha a bagunça...").grid(column=2, row=1)
-        self.nome = ttk.Entry(self.frm,  text="Your name",).grid(column=2, row=2)
-        self.chave = ttk.Entry(self.frm,  text="Your Key",).grid(column=2, row=3)
-        self.bfim = ttk.Button(self.frm, text="Quit", command=janin.destroy).grid(column=2, row=4)
+        self.nome = ttk.Entry(self.frm0, text="Your name").grid(column=2, row=2)
+        self.chave = ttk.Entry(self.frm0, text="Your Key", show="*").grid(column=2, row=3)
+        self.bfim = ttk.Button(self.frm0, text="Quit", command=self.bclicked_fim).grid(column=2, row=4)
+        self.bok = ttk.Button(self.frm0, text="OK", command=self.bclicked_logar).grid(column=1, row=4)
+        mainloop()
 
-
-#     def findar():
+    def bclicked_fim(self):
+        self.frm.quit()
         
-#     # def limpar():
+    def bclicked_logar(self):
+        return self.nome, self.chave
+
+    def opera(self):
+        self.nick = self.nome
+        
+        self.frm = ttk.Frame(self.tk, padding=10)
+        self.grade1 = self.frm1.grid()
 
 
+Janela()
+ok = Janela.bclicked_logar
+if ok != None:
+    Janela.opera
+else:
+    print("Encerrado por falta de usuário.")
+    Janela.bclicked_fim
 
-janin = Tk()
-
-Janela(janin)
-
-janin.mainloop()
+# mainloop()
